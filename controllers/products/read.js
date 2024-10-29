e13a54cf51fda5f201c3f6fb9628d4483d0ce5fd
Carlos Barrios <cabarriosb@gmail.com>
2024-10-29 16:48:14 -0500

    Build Backm

--- controllers/products/read.js
+++ controllers/products/read.js
@@ -1,4 +1,4 @@
-import Product from "../../models/Product.js";
+import Product,{initializeProductData} from "../../models/Product.js";
 
 export const getProducts = async (req, res) => {
     try {
@@ -8,5 +8,25 @@ export const getProducts = async (req, res) => {
         })
     } catch (error) {
         res.status(404).json({ message: error.message });
+    }
+}
+
+export const createProduct = async (req, res) => {
+    try {
+
+        const product =  initializeProductData(req.body)
+        const savedProduct = await product.save();
+
+        // Responder con el producto creado
+        return res.status(201).json({
+            message: "Product created successfully",
+            product: savedProduct
+        });
+
+    } catch (error) {
+        return res.status(404).json({
+            message: "Error creating product",
+            error: error.message
+        });
     }
 }
--- router/products.js
+++ router/products.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { getProducts } from "../controllers/products/read.js";   
+import { getProducts, createProduct } from "../controllers/products/read.js";   
 
 
 const router = Router();    
 router.get("/all", getProducts);
+router.post("/create", createProduct);
 export default router
